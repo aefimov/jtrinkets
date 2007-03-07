b9d4c64330847ec12a74f01e410be5c7a031effa
aefimov.box <aefimov.box@ca09c16b-132b-0410-878d-17bc72540b1c>
2007-03-07 16:51:20 +0000

    Put first version

---
+++ ui/jcalendar/src/org/trinkets/ui/plaf/CalendarUI.java
@@ -0,0 +1,11 @@
+package org.trinkets.ui.plaf;
+
+import javax.swing.plaf.ComponentUI;
+
+/**
+ * UI for {@link org.trinkets.ui.JCalendar}
+ *
+ * @author Alexey Efimov
+ */
+public abstract class CalendarUI extends ComponentUI {
+}

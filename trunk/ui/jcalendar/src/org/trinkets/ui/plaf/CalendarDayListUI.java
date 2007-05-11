b393d27095c85d8815fb092e6f5bd8aab9272f38
aefimov.box <aefimov.box@ca09c16b-132b-0410-878d-17bc72540b1c>
2007-05-11 13:51:47 +0000

    Add mouse events to component

--- trunk/ui/jcalendar/src/org/trinkets/ui/plaf/CalendarDayListUI.java
+++ trunk/ui/jcalendar/src/org/trinkets/ui/plaf/CalendarDayListUI.java
@@ -1,6 +1,10 @@
 package org.trinkets.ui.plaf;
 
+import org.jetbrains.annotations.NotNull;
+import org.trinkets.ui.JCalendarDayList;
+
 import javax.swing.plaf.ComponentUI;
+import java.awt.*;
 
 /**
  * UI for {@link org.trinkets.ui.JCalendarDayList}
@@ -8,4 +12,6 @@ import javax.swing.plaf.ComponentUI;
  * @author Alexey Efimov
  */
 public abstract class CalendarDayListUI extends ComponentUI {
+    @NotNull
+    public abstract Point toCellPoint(@NotNull JCalendarDayList c, @NotNull Point point);
 }

55425a21dd0a120c38292d6f1ae0c480bfd38fbf
aefimov.box <aefimov.box@ca09c16b-132b-0410-878d-17bc72540b1c>
2007-05-14 08:53:45 +0000

    Add setSelectedDate/getSelectedDate, cosmetic fixes

--- trunk/ui/jcalendar/src/org/trinkets/ui/CalendarDayListModel.java
+++ trunk/ui/jcalendar/src/org/trinkets/ui/CalendarDayListModel.java
@@ -11,8 +11,8 @@ public interface CalendarDayListModel {
     /**
      * Return representation of day date for week relative to current month.
      *
-     * @param weekIndex    week index from -1 to getWeeksInMonth() + 1.
-     * @param weekDayIndex Number of week day.
+     * @param weekIndex    week index from <code>-1</code> to <code>getWeeksInMonth() + 1</code>.
+     * @param weekDayIndex Number of week day (from <code>0</code>).
      * @return String of day date - 1, 2 .. 31.
      */
     String getDayCellValue(int weekIndex, int weekDayIndex);
@@ -42,7 +42,7 @@ public interface CalendarDayListModel {
     /**
      * Return <code>true</code> if cell is a weekend.
      *
-     * @param weekDayIndex Number of week day.
+     * @param weekDayIndex Number of week day (from <code>0</code>).
      * @return <code>true</code> if cell is a weekend.
      */
     boolean isWeekendCell(int weekDayIndex);
@@ -50,8 +50,8 @@ public interface CalendarDayListModel {
     /**
      * Return <code>true</code> if cell within current month.
      *
-     * @param weekIndex    week index from -1 to getWeeksInMonth() + 1.
-     * @param weekDayIndex Number of week day.
+     * @param weekIndex    week index from <code>-1</code> to <code>getWeeksInMonth() + 1</code>.
+     * @param weekDayIndex Number of week day (from <code>0</code>).
      * @return <code>true</code> if cell within current month.
      */
     boolean isWithinMonthCell(int weekIndex, int weekDayIndex);
@@ -59,9 +59,34 @@ public interface CalendarDayListModel {
     /**
      * Return Calendar value of cell.
      *
-     * @param weekIndex    week index from -1 to getWeeksInMonth() + 1.
-     * @param weekDayIndex Number of week day.
+     * @param weekIndex    week index from <code>-1</code> to <code>getWeeksInMonth() + 1</code>.
+     * @param weekDayIndex Number of week day (from <code>0</code>).
      * @return Calendar value of cell.
      */
     Calendar getValueAt(int weekIndex, int weekDayIndex);
+
+    /**
+     * Return tooltip for value of cell.
+     *
+     * @param weekIndex    week index from <code>-1</code> to <code>getWeeksInMonth() + 1</code>.
+     * @param weekDayIndex Number of week day (from <code>0</code>).
+     * @return Tooltip for value of cell.
+     */
+    String getToolTipTextAt(int weekIndex, int weekDayIndex);
+
+    /**
+     * Return week index of date
+     *
+     * @param calendar Calendar
+     * @return Week index from <code>-1</code> to <code>getWeeksInMonth() + 1</code>.
+     */
+    int weekIndexOf(Calendar calendar);
+
+    /**
+     * Return week day index of date
+     *
+     * @param calendar Calendar
+     * @return Week day index from <code>0</code>
+     */
+    int weekDayIndexOf(Calendar calendar);
 }
--- trunk/ui/jcalendar/src/org/trinkets/ui/plaf/CalendarDayListUI.java
+++ trunk/ui/jcalendar/src/org/trinkets/ui/plaf/CalendarDayListUI.java
@@ -12,6 +12,13 @@ import java.awt.*;
  * @author Alexey Efimov
  */
 public abstract class CalendarDayListUI extends ComponentUI {
-    @NotNull
     public abstract Point toCellPoint(@NotNull JCalendarDayList c, @NotNull Point point);
+
+    public abstract void setHoverCell(@NotNull JCalendarDayList c, Point point);
+
+    public abstract void setSelectionCell(@NotNull JCalendarDayList c, Point point);
+
+    public abstract Point getHoverCell(@NotNull JCalendarDayList c);
+
+    public abstract Point getSelectionCell(@NotNull JCalendarDayList c);
 }

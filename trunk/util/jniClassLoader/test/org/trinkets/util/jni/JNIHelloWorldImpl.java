4882ef5a048df1fb8c998be63396ed194fdea1e7
aefimov.box <aefimov.box@ca09c16b-132b-0410-878d-17bc72540b1c>
2007-06-22 15:27:25 +0000

    Support for parameters in constructors. Refactored JNIBundleLoader.

--- trunk/util/jniClassLoader/test/org/trinkets/util/jni/JNIHelloWorldImpl.java
+++ trunk/util/jniClassLoader/test/org/trinkets/util/jni/JNIHelloWorldImpl.java
@@ -3,16 +3,24 @@ package org.trinkets.util.jni;
 import org.trinkets.util.jni.annotations.JNIBundle;
 import org.trinkets.util.jni.annotations.JNILibrary;
 
+import java.text.MessageFormat;
+
 @JNILibrary("JNIHelloWorld")
 @JNIBundle("JNIHelloWorldBundle.jar")
 public final class JNIHelloWorldImpl implements JNIHelloWorld {
+    private final String user;
+
+    public JNIHelloWorldImpl(String me) {
+        this.user = me;
+    }
+
     /**
      * Here is public wrapper to native method.
      *
      * @param hello Hello string
      */
     public void sayHello(String hello) {
-        sayHello0(hello);
+        sayHello0(MessageFormat.format("{0}>{1}", user, hello));
     }
 
     /**
